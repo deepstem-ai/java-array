70259bac9094dd5363a7e3b92b8c1f60dd3cfe0e
deepstem-ai <deepstem.ai@gmail.com>
2024-06-24 10:35:31 +0700

    Add files via upload

---
+++ scratch_2.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+
+        // create an array
+        int[] age = {12, 4, 5, 152, 954, 895, 512, 654, 1045};
+
+        // loop through the array
+        // using for loop
+        System.out.println("Using for-each Loop:");
+        for(int i : age) {
+            System.out.println(i);
+        }
+    }
+}
